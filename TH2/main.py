4390148d8d3a38e92091e8f980fdb18938aeed80
DanQuang <21521917@gm.uit.edu.vn>
2023-12-18 21:29:16 +0700

    update

--- TH2/main.py
+++ TH2/main.py
@@ -12,11 +12,11 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.manual_seed(42)
 
 # Load data
-train_dataset = dataset.FashionMNISTDataset("C:/Users/tquan/OneDrive/Desktop/DS201/TH2/Data/train-images-idx3-ubyte",
-                                            "C:/Users/tquan/OneDrive/Desktop/DS201/TH2/Data/train-labels-idx1-ubyte")
+train_dataset = dataset.FashionMNISTDataset("D:/Project/DS201/TH2/Data/train-images-idx3-ubyte",
+                                            "D:/Project/DS201/TH2/Data/train-labels-idx1-ubyte")
 
-test_dataset = dataset.FashionMNISTDataset("C:/Users/tquan/OneDrive/Desktop/DS201/TH2/Data/t10k-images-idx3-ubyte",
-                                           "C:/Users/tquan/OneDrive/Desktop/DS201/TH2/Data/t10k-labels-idx1-ubyte")
+test_dataset = dataset.FashionMNISTDataset("D:/Project/DS201/TH2/Data/t10k-images-idx3-ubyte",
+                                           "D:/Project/DS201/TH2/Data/t10k-labels-idx1-ubyte")
 
 # Load Dataloader
 train_dataloader = DataLoader(train_dataset, 64, True, collate_fn= utils.collate_fn)
@@ -26,9 +26,18 @@ test_dataloader = DataLoader(test_dataset, 32, True, collate_fn= utils.collate_f
 model_0 = FashionMNISTModel().to(device)
 
 # Loss and optimizer, Accuracy, Precision, Recall, F1-score
+# SGD
 optimizer = optim.SGD(model_0.parameters(),
                       lr = 0.01,
                       momentum= 0.5)
+# # RMSrpop
+# optimizer = optim.RMSprop(model_0.parameters(),
+#                           lr= 0.01,
+#                           momentum= 0.9)
+# # Adam
+# optimizer = optim.Adam(model_0.parameters(),
+#                           lr= 1e-5)
+
 loss_fn = nn.CrossEntropyLoss().to(device)
 Acc_fn = Accuracy(task= "multiclass", num_classes= 10)
 Prec_fn = Precision(task= "multiclass", num_classes= 10, average= "macro")
@@ -50,6 +59,10 @@ for epoch in range(5):
         # Forward
         y_logits = model_0(X)
         loss = loss_fn(y_logits, y)
+        
+        # # L2 Regularization - Use if need use L2
+        # loss = loss_fn(y_logits, y) + model_0.l2_regularization()
+
         loss_all += loss.item()
 
         # Backward
--- TH2/model/FashionMNISTModel.py
+++ TH2/model/FashionMNISTModel.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 
 class FashionMNISTModel(nn.Module):
     def __init__(self):
@@ -13,6 +14,12 @@ class FashionMNISTModel(nn.Module):
                               out_features= 256)
         self.MLP3 = nn.Linear(in_features= 256,
                               out_features= 10)
+        
+        # # W and b use Ones init
+        # self.init_weights_1()
+
+        # # W use Xavier and b use Ones
+        # self.init_weights_2()
 
     def forward(self, x: torch.Tensor):
         x = self.flatten(x)
@@ -22,3 +29,26 @@ class FashionMNISTModel(nn.Module):
         x = self.relu(x)
         x = self.MLP3(x)
         return x
+    
+    def l2_regularization(self, lambda_l2= 0.01):
+        l2_loss = 0
+        for param in self.parameters():
+            l2_loss += torch.norm(param, p=2)
+
+        return lambda_l2 * l2_loss
+    
+    def init_weights_1(self):
+        init.ones_(self.MLP1.weight)
+        init.ones_(self.MLP1.bias)
+        init.ones_(self.MLP2.weight)
+        init.ones_(self.MLP2.bias)
+        init.ones_(self.MLP3.weight)
+        init.ones_(self.MLP3.bias)
+
+    def init_weights_2(self):
+        init.xavier_uniform_(self.MLP1.weight)
+        init.zeros_(self.MLP1.bias)
+        init.xavier_uniform_(self.MLP2.weight)
+        init.zeros_(self.MLP2.bias)
+        init.xavier_uniform_(self.MLP3.weight)
+        init.zeros_(self.MLP3.bias)
