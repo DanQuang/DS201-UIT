ebc37b1234e7fac5ae9d6288787922e314469d65
DanQuang <21521917@gm.uit.edu.vn>
2023-12-31 17:51:39 +0700

    fix evaluate

--- TH4/evaluate/evaluate.py
+++ TH4/evaluate/evaluate.py
@@ -2,8 +2,8 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 def compute_score(y_true, y_pred):
     accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred, average= 'macro')
-    recall = recall_score(y_true, y_pred, average= 'macro')
-    f1 = f1_score(y_true, y_pred, average= 'macro')
+    precision = precision_score(y_true, y_pred, average= 'macro', zero_division= 0)
+    recall = recall_score(y_true, y_pred, average= 'macro', zero_division= 0)
+    f1 = f1_score(y_true, y_pred, average= 'macro', zero_division= 0)
 
     return accuracy, precision, recall, f1
