58f5aa539100bf9ef5b990e654e6164cc422950d
DanQuang <21521917@gm.uit.edu.vn>
2024-01-03 21:30:13 +0700

    fix

--- TH5/data_utils/load_data.py
+++ TH5/data_utils/load_data.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 from data_utils.vocab import Vocab
 from data_utils.utils import preprocess_sentence
 import pandas as pd
+import os
 
 class MyDataset(Dataset):
     def __init__(self, data_path, vocab= None):
@@ -13,16 +14,19 @@ class MyDataset(Dataset):
 
         self.vocab = vocab
 
-        for sample in data:
-            self.sentences.append(sample["sentence"])
-            self.sentiments.append(sample["sentiment"])
+        for i in range(len(data)):
+            self.sentences.append(data.iloc[i]["sentence"])
+            self.sentiments.append(data.iloc[i]["sentiment"])
         
 
     def __len__(self):
         return len(self.sentences)
     
     def __getitem__(self, idx):
-        return self.sentences[idx], self.sentiments[idx]
+        return {
+            "sentence":self.sentences[idx], 
+            "label":self.sentiments[idx]
+        }
     
 class Load_data:
     def __init__(self, config):
@@ -30,13 +34,14 @@ class Load_data:
         self.dev_batch = config["valid_batch"]
         self.test_batch = config["test_batch"]
 
+        self.dataset_folder = config['dataset']['dataset_folder']
         self.train_path = config['dataset']["train_path"]
         self.dev_path = config['dataset']["valid_path"]
         self.test_path = config['dataset']["test_path"]
 
     def load_train_dev(self):
-        train_dataset = MyDataset(self.train_path)
-        val_dataset = MyDataset(self.dev_path)
+        train_dataset = MyDataset(os.path.join(self.dataset_folder, self.train_path))
+        val_dataset = MyDataset(os.path.join(self.dataset_folder, self.dev_path))
 
         train_dataloader = DataLoader(train_dataset, self.train_batch, shuffle= True)
         dev_dataloader = DataLoader(val_dataset, self.dev_batch, shuffle= False)
@@ -44,6 +49,6 @@ class Load_data:
         return train_dataloader, dev_dataloader
     
     def load_test(self):
-        test_dataset = MyDataset(self.test_path)
+        test_dataset = MyDataset(os.path.join(self.dataset_folder, self.test_path))
         test_dataloader = DataLoader(test_dataset, self.test_batch, shuffle= False)
         return test_dataloader
